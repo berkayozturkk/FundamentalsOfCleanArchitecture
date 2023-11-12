e2db4298f64633019eade2508dab640d76800876
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-11-12 20:02:36 +0300

    Assembly register of the presentation layer.

---
+++ src/Extarnel/CleanArchitecture.Presentation/AssemblyReferance.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace CleanArchitecture.Presentation;
+
+public static class AssemblyReferance
+{
+    public static readonly Assembly Assembly = typeof(Assembly).Assembly;
+}
---
+++ src/Extarnel/CleanArchitecture.Presentation/Controllers/ValuesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Presentation.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ValuesController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("ValuesController Api Connection Successful.");
+    }
+}
