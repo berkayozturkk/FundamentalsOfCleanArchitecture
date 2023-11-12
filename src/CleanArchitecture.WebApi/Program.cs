5ee61b8f122fea8d76509751929f7683eaab6380
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-11-13 00:11:22 +0300

    CarService added.

--- src/CleanArchitecture.WebApi/Program.cs
+++ src/CleanArchitecture.WebApi/Program.cs
@@ -1,7 +1,11 @@
+using CleanArchitecture.Application.Services;
 using CleanArchitecture.Persistance.Context;
+using CleanArchitecture.Persistance.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<ICarService,CarService>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
--- src/Core/CleanArchitecture.Application/Features/CarFeatures/Command/CreateCar/CreateCarCommandHandler.cs
+++ src/Core/CleanArchitecture.Application/Features/CarFeatures/Command/CreateCar/CreateCarCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using CleanArchitecture.Domain.Dtos;
+﻿using CleanArchitecture.Application.Services;
+using CleanArchitecture.Domain.Dtos;
 using MediatR;
 
 namespace CleanArchitecture.Application.Features.CarFeatures.Command.CreateCar;
@@ -6,8 +7,16 @@ namespace CleanArchitecture.Application.Features.CarFeatures.Command.CreateCar;
 public sealed class CreateCarCommandHandler
     : IRequestHandler<CreateCarCommand, MessageResponse>
 {
+    private readonly ICarService _carService;
+
+    public CreateCarCommandHandler(ICarService carService)
+    {
+        _carService = carService;
+    }
+
     public async Task<MessageResponse> Handle(CreateCarCommand request, CancellationToken cancellationToken)
     {
+        await _carService.CreateAsync(request, cancellationToken);
         return new("Car Added",true,null);
     }
 }
