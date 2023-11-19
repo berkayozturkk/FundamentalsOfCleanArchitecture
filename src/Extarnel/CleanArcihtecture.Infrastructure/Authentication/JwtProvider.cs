ab1ee973dcda65544ace9cca76eafe7e1c9201a5
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-11-19 17:40:23 +0300

    CreateToken

---
+++ src/Extarnel/CleanArcihtecture.Infrastructure/Authentication/JwtProvider.cs
@@ -0,0 +1,42 @@
+﻿using CleanArchitecture.Application.Abstractions;
+using CleanArchitecture.Domain.Entities;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CleanArcihtecture.Infrastructure.Authentication;
+
+public sealed class JwtProvider : IJwtProvider
+{
+    private readonly JwtOption _jwtOptions;
+
+    public JwtProvider(IOptions<JwtOption> jwtOptions)
+    {
+        _jwtOptions = jwtOptions.Value;
+    }
+
+    public string CreateToken(AppUser user)
+    {
+        var claims = new Claim[]
+        {
+            new Claim(ClaimTypes.Email,user.Email),
+            new Claim(JwtRegisteredClaimNames.Name,user.UserName),
+            new Claim("UserName",user.UserName),
+        };
+
+        JwtSecurityToken jwtSecurityToken = new(
+            issuer: _jwtOptions.Issuer,
+            audience: _jwtOptions.Audience,
+            claims: claims,
+            notBefore:DateTime.Now,
+            expires:DateTime.Now.AddHours(1),
+            signingCredentials: new SigningCredentials(new SymmetricSecurityKey
+            (Encoding.UTF8.GetBytes(_jwtOptions.SecretKey)),SecurityAlgorithms.HmacSha256));
+
+        string token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken); 
+       
+        return token;
+    }
+}
